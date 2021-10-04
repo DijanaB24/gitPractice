b8d8893ca773b10176ffcb7a37149e79bed9e060
dijan <dijanamarkic@yahoo.com>
2021-10-04 17:30:15 -0500

    added class Food

---
+++ src/Food.java
@@ -0,0 +1,5 @@
+public class Food {
+    /*
+    In the context of software or information modeling, a happy path (sometimes called happy flow) is a default scenario featuring no exceptional or error conditions.[1][2] For example, the happy path for a function validating credit card numbers would be where none of the validation rules raise an error, thus letting execution continue successfully to the end, generating a positive response.
+     */
+}
