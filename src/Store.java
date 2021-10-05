c680e362329b290397aa1ae27007e1714b994c2e
dijan <dijanamarkic@yahoo.com>
2021-10-05 11:31:37 -0500

    added Store class

---
+++ src/Store.java
@@ -0,0 +1,9 @@
+public class Store {
+    public static void main(String[] args) {
+        //making new class called Store
+
+        System.out.println("Store class has been made successfully");
+
+
+    }
+}
